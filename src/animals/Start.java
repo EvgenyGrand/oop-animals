3366db6cef9ebe4b0e9cf4a9aa912ae9eb072769
evgen <Evgeny.Grand@mail.ru>
2022-10-02 18:30:11 +0300

    remove null

--- src/animals/Start.java
+++ src/animals/Start.java
@@ -1,6 +1,7 @@
 package animals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -15,7 +16,7 @@ public class Start {
 
 
         while (true) {
-            System.out.println("Enter command : ");
+            System.out.println("Введите команду из списка : ADD/LIST/EXIT : ");
             Command selection = Command.valueOf(sc.next().trim().toUpperCase());
 
             switch (selection) {
@@ -23,6 +24,7 @@ public class Start {
                     animals.add(createAnimal());
                     break;
                 case LIST:
+                    animals.removeAll(Arrays.asList(null,""));
                     for (Animal animal : animals) {
                         System.out.println(animal);
                     }
@@ -37,64 +39,61 @@ public class Start {
     }
 
     protected static Animal createAnimal() {
+
         System.out.println("Введите животное : cat/dog/duck");
         String animalType = sc.next().toLowerCase().trim();
         if (animalType.equals("cat")) {
             System.out.println("Введите имя");
             String name = sc.next();
-            System.out.println("Введите возраст");
+            System.out.println("Введите возраст в диапазоне от 0 до 30 лет!");
             int age = sc.nextInt();
-            if (age > 1 && age < 30) {
-                System.out.println("Возраст введен корректно!");
-            } else {
-                System.out.println("Возраст введен некорректно!, Введите возраст в диапазоне от 1 года до 30 лет");
-                System.exit(0);
+            while (age > 0 && age < 30) {
+//                if (age > 0 && age < 30) {
+                    System.out.println("Возраст введен корректно!");
+                    System.out.println("Введите вес");
+                    int weight = sc.nextInt();
+                    System.out.println("Введите цвет");
+                    String color = sc.next();
+                    Cat cat = new Cat(name, age, weight, color);
+                    cat.say();
+                    return cat;
+
             }
-            System.out.println("Введите вес");
-            int weight = sc.nextInt();
-            System.out.println("Введите цвет");
-            String color = sc.next();
-            Cat cat = new Cat(name, age, weight, color);
-            cat.say();
-            return cat;
 
         } else if (animalType.equals("dog")) {
             System.out.println("Введите имя");
             String name = sc.next();
-            System.out.println("Введите возраст");
+            System.out.println("Введите возраст в диапазоне от 0 до 30 лет");
             int age = sc.nextInt();
-            if (age > 1 && age < 30) {
+            while (age > 0 && age < 30) {
+//                if (age > 0 && age < 30) {
                 System.out.println("Возраст введен корректно!");
-            } else {
-                System.out.println("Возраст введен некорректно!, Введите возраст в диапазоне от 1 года до 30 лет");
-                System.exit(0);
+                System.out.println("Введите вес");
+                int weight = sc.nextInt();
+                System.out.println("Введите цвет");
+                String color = sc.next();
+                Dog dog = new Dog(name, age, weight, color);
+                dog.say();
+                return dog;
             }
-            System.out.println("Введите вес");
-            int weight = sc.nextInt();
-            System.out.println("Введите цвет");
-            String color = sc.next();
-            Dog dog = new Dog(name, age, weight, color);
-            dog.say();
-            return dog;
+
 
         } else if (animalType.equals("duck")) {
             System.out.println("Введите имя");
             String name = sc.next();
-            System.out.println("Введите возраст");
+            System.out.println("Введите возраст в диапазоне от 0 до 5 лет");
             int age = sc.nextInt();
-            if (age > 1 && age < 5) {
-                System.out.println("Возраст введен корректно!");
-            } else {
-                System.out.println("Возраст введен корректно!, Введите возраст в диапазоне от 1 года до 5 лет");
-                System.exit(0);
+            while (age > 0 && age < 5) {
+//                if (age > 0 && age < 5) {
+                    System.out.println("Возраст введен корректно!");
+                System.out.println("Введите вес");
+                int weight = sc.nextInt();
+                System.out.println("Введите цвет");
+                String color = sc.next();
+                Duck duck = new Duck(name, age, weight, color);
+                duck.say();
+                return duck;
             }
-            System.out.println("Введите вес");
-            int weight = sc.nextInt();
-            System.out.println("Введите цвет");
-            String color = sc.next();
-            Duck duck = new Duck(name, age, weight, color);
-            duck.say();
-            return duck;
         }
         return null;
     }
